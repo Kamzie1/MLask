1e87663bdc7b5f287763fa0c7e8674d351c172f4
Kamil Zielonka <kamilzie07@gmail.com>
2026-06-01 23:23:34 +0200

    update link

--- tests/ActivationFunction.cpp
+++ tests/ActivationFunction.cpp
@@ -6,7 +6,6 @@ using namespace mlask;
 int main(){
     Model model(1, 4);
     model.addFullyConnectedLayer<1,4>();
-    model.addActivationFunction(InternalActivationFunction::Relu);
     model.addLambdaActivationFunction([](float_t x){ return 0.5*x*x; }, [](float_t x){ return x; } );
     model.addLayer<Relu>();
     model.forward(vectorIn{{0}});
